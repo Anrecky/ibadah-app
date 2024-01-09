27991ce272e60b1fac4a41f861f37628d5bdaa9e
Recky Anderson <reckyanderson@gmail.com>
2024-01-09 10:26:30 +0700

    add dark mode feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,38 +35,60 @@
         </main>
     </div>
     <script>
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
+
+    <script>
         document.addEventListener('DOMContentLoaded', function() {
             window.feather.replace();
+            var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+            var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-            const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-            const themeToggleBtn = document.getElementById('theme-toggle');
-            const themeToggleIcon = document.getElementById('theme-toggle-icon');
+            // Change the icons inside the button based on previous settings
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window
+                    .matchMedia('(prefers-color-scheme: dark)').matches)) {
+                themeToggleLightIcon.classList.remove('hidden');
+            } else {
+                themeToggleDarkIcon.classList.remove('hidden');
+            }
+
+            var themeToggleBtn = document.getElementById('theme-toggle');
 
             themeToggleBtn.addEventListener('click', function() {
-                const colorTheme = localStorage.getItem('color-theme');
 
-                if (colorTheme === 'dark' || colorTheme === null) {
-                    localStorage.setItem('color-theme', 'light');
+                // toggle icons inside button
+                themeToggleDarkIcon.classList.toggle('hidden');
+                themeToggleLightIcon.classList.toggle('hidden');
+
+                // if set via local storage previously
+                if (localStorage.getItem('color-theme')) {
+                    if (localStorage.getItem('color-theme') === 'light') {
+                        document.documentElement.classList.add('dark');
+                        localStorage.setItem('color-theme', 'dark');
+                    } else {
+                        document.documentElement.classList.remove('dark');
+                        localStorage.setItem('color-theme', 'light');
+                    }
+
+                    // if NOT set via local storage previously
                 } else {
-                    localStorage.setItem('color-theme', 'dark');
+                    if (document.documentElement.classList.contains('dark')) {
+                        document.documentElement.classList.remove('dark');
+                        localStorage.setItem('color-theme', 'light');
+                    } else {
+                        document.documentElement.classList.add('dark');
+                        localStorage.setItem('color-theme', 'dark');
+                    }
                 }
 
-                // Update the dark mode icon
-                setupDarkModeIcon(localStorage.getItem('color-theme'));
-                document.documentElement.classList.toggle('dark', colorTheme === 'dark' ? true : false);
             });
 
-            // Initial setup of dark mode icon
-            setupDarkModeIcon(localStorage.getItem('color-theme'));
-
-            function setupDarkModeIcon(colorTheme) {
-                if (colorTheme === 'dark') {
-                    themeToggleIcon.setAttribute('data-feather', 'moon');
-                } else {
-                    themeToggleIcon.setAttribute('data-feather', 'sun');
-                }
-                window.feather.replace();
-            }
         });
     </script>
 
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -32,6 +32,9 @@ new class extends Component {
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Kelola User') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -92,6 +95,15 @@ new class extends Component {
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                {{ __('Hibah') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                {{ __('Jenis Hibah') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                {{ __('Kelola User') }}
             </x-responsive-nav-link>
         </div>
 
