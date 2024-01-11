df0f3f3c204f6c2981fad63f1056d25ddae60d58
Recky Anderson <reckyanderson@gmail.com>
2024-01-11 19:10:35 +0700

    add role seeder spatie permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class
+        ]);
 
         \App\Models\User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@mail.com',
-        ]);
+        ])->assignRole('admin');
     }
 }
