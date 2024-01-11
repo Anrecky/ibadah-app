a4ecb0ad43cfbd5652ba16f17ead71005cd6c62f
Recky Anderson <reckyanderson@gmail.com>
2024-01-11 19:12:00 +0700

    add manage user blade & finish CRUD feature

--- resources/views/livewire/manage-user.blade.php
+++ resources/views/livewire/manage-user.blade.php
@@ -2,11 +2,81 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
+                <x-modal name="add-user-modal" :show="$errors->isNotEmpty()" focusable>
+                    <h1 class="mx-6 mt-6 font-bold text-xl capitalize">Tambah User Baru</h1>
+                    <hr class="my-2" />
+                    <form wire:submit="addUser" class="p-6">
+                        <!-- Name -->
+                        <div>
+                            <x-input-label for="name" :value="__('Name')" />
+                            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text"
+                                name="name" required autofocus autocomplete="name" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
 
-                <div class="w-fit ml-auto mb-2">
-                    <form wire:submit="search">
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-input-label for="email" :value="__('Email')" />
+                            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email"
+                                name="email" required autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mt-4">
+                            <x-input-label for="password" :value="__('Password')" />
+
+                            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password"
+                                name="password" required autocomplete="new-password" />
+
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+
+                        <!-- Confirm Password -->
+                        <div class="mt-4">
+                            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                            <x-text-input wire:model="password_confirmation" id="password_confirmation"
+                                class="block mt-1 w-full" type="password" name="password_confirmation" required
+                                autocomplete="new-password" />
+
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+
+                            <x-primary-button class="ms-4">
+                                {{ __('Register') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </x-modal>
+                <x-primary-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'add-user-modal')"
+                    class="my-2"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="feather feather-plus w-3.5 h-3.5 mr-1">
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg> User</x-primary-button>
+                <div class="flex justify-between sm:mb-5 items-center flex-col sm:flex-row gap-3 mb-10">
+
+                    <div class="flex items-center gap-1 justify-start text-sm">
+                        <p>Menampilkan</p>
+                        <select wire:model.live="perPage"
+                            class="outline-none bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 p-1">
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+                        <p>data</p>
+                    </div>
+
+
+                    <div>
                         <label for="table-search" class="sr-only">Cari</label>
-                        <div class="relative mt-1 inline-block">
+                        <div class="relative mt-1 inline-block ">
                             <div
                                 class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
@@ -15,25 +85,22 @@
                                         stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
-                            <input wire:model="query" type="text"
-                                class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            <input wire:model.live.debounce.350ms="searchTerm" type="text"
+                                class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded sm:w-80 w-50 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Cari users...">
                         </div>
-                        <button
-                            class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-                            type="submit">Cari User</button>
-                    </form>
+                    </div>
                 </div>
 
                 <div class="overflow-x-auto relative mb-10">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <thead class="text-xs text-white uppercase bg-blue-600 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="p-4">
                                     <div class="flex items-center">
-                                        <input id="checkbox-all-search" type="checkbox"
+                                        <input id="checkbox-all" wire:model="selectPage" type="checkbox"
                                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                        <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                                        <label for="checkbox-all" class="sr-only">checkbox</label>
                                     </div>
                                 </th>
                                 <th scope="col" class="px-6 py-3">
@@ -52,11 +119,12 @@
                         </thead>
                         <tbody>
                             @foreach ($users as $user)
-                                <tr
+                                <tr wire:key="{{ $user->id }}"
                                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <td class="w-4 p-4">
                                         <div class="flex items-center">
-                                            <input id="checkbox-table-search-1" type="checkbox"
+                                            <input wire:model.change="selectedUsers" value="{{ $user->id }}"
+                                                type="checkbox"
                                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                             <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
                                         </div>
@@ -65,8 +133,48 @@
                                         class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         {{ $user->name }}</th>
                                     <td class="px-6 py-4">{{ $user->email }}</td>
-                                    <td class="px-6 py-4">Admin</td>
-                                    <td class="px-6 py-4">EDIT | DELETE</td>
+                                    <td class="px-6 py-4">
+                                        @foreach ($user->roles as $role)
+                                            <span
+                                                class="{{ $role->name === 'admin' ? 'bg-blue-600' : 'bg-teal-600' }} text-white px-2 py-1 rounded">
+                                                {{ Str::ucfirst($role->name) }}
+                                            </span>
+                                        @endforeach
+                                    </td>
+                                    <td class="px-6 py-4 flex gap-2">
+                                        <button type="button"
+                                            class="text-black bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-yellow-300 dark:hover:bg-yellow-200 dark:focus:ring-yellow-300">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-edit w-3.5 h-3.5 me-2">
+                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+                                                </path>
+                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z">
+                                                </path>
+                                            </svg>
+                                            Edit
+                                        </button>
+                                        <!-- TODO: Improve this confirm dialog -->
+                                        <button wire:confirm="Apakah anda yakin ingin menghapus data user ini?"
+                                            wire:click="deleteUser({{ $user->id }})" type="button"
+                                            class="text-white bg-red-600 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-red-600 dark:hover:bg-red-500 dark:focus:ring-red-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-trash-2 w-3.5 h-3.5 me-2">
+                                                <polyline points="3 6 5 6 21 6"></polyline>
+                                                <path
+                                                    d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                </path>
+                                                <line x1="10" y1="11" x2="10" y2="17">
+                                                </line>
+                                                <line x1="14" y1="11" x2="14" y2="17">
+                                                </line>
+                                            </svg>
+                                            Hapus
+                                        </button>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
